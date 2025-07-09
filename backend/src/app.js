02a52d0da6d0a0d188f1c030feba5527939904a4
ProgrammerRimon <83152804+Rimon360@users.noreply.github.com>
2025-07-10 00:48:25 +0600

    init

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ app.get("/api/verify-token", verifyToken, (req, res) => {
 app.use('/api/shops', shopRoutes)
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Technical Error!. Please try again later!", d: req.originalUrl });
+  res.status(404).json({ message: "Technical Error!. Please try again later!", d: req });
 });
 
 // module.exports = app;
