15a9fc8460b181e435b22b3d6c133d461855808c
ProgrammerRimon <83152804+Rimon360@users.noreply.github.com>
2025-10-26 02:32:07 +0600

    init

--- backend/src/utils/util.js
+++ backend/src/utils/util.js
@@ -77,8 +77,7 @@ function getPort(min = 40000, max = 60000) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-async function sendOtpEmail(toEmail) {
-  console.log(process.env.SENDGRID_API_KEY);
+async function sendOtpEmail(toEmail) { 
   let otp = getPort(111111, 999999)
   const msg = {
     to: toEmail,
